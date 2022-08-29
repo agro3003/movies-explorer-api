323740c52d43e15a8af51a7923db9f31df1d84c3
Roman Kononov <doubleempty@yandex.ru>
2022-08-29 22:21:39 +0300

    change validate

--- routes/movies.js
+++ routes/movies.js
@@ -13,16 +13,16 @@ router.get('/movies', getUserMovies);
 router.post('/movies', celebrate({
   body: Joi.object().keys({
     country: Joi.string().min(2).max(30).required(),
-    director: Joi.string().min(2).max(30).required(),
+    director: Joi.string().min(2).max(130).required(),
     duration: Joi.number().required(),
     year: Joi.string().min(2).max(30).required(),
-    description: Joi.string().min(2).max(30).required(),
+    description: Joi.string().min(2).max(3000).required(),
     image: Joi.string().required().custom(validateURL),
     trailerLink: Joi.string().required().custom(validateURL),
     thumbnail: Joi.string().required().custom(validateURL),
     movieId: Joi.number().required(),
-    nameRU: Joi.string().min(2).max(30).required(),
-    nameEN: Joi.string().min(2).max(30).required(),
+    nameRU: Joi.string().min(2).max(300).required(),
+    nameEN: Joi.string().min(2).max(300).required(),
   }),
 }), createMovie);
 
